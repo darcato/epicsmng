74418be37f1f2628264f1acb383a2bc8ec37b954
Davide Marcato <davide.marcato@lnl.infn.it>
2018-05-25 14:06:50 +0200

    Fixed autocomplete

--- epicsmng-completion.bash
+++ epicsmng-completion.bash
@@ -1,2 +1,20 @@
 #/usr/bin/env bash
-complete -W "makemodules cleanmodules configureioc" epicsmng
+
+# the commands available as first argument of epicsmng
+commands="makemodules cleanmodules listmodules createconf configureioc"
+
+#custom function to autocomplete epicsmng
+function _epicsmng_autocomplete_()
+{
+    #if typing the first argument -> autocomplete with one of the commands
+    #in all the other cases, fallback to default
+    case $COMP_CWORD in
+        1) COMPREPLY=($(compgen -W "$commands" -- ${COMP_WORDS[COMP_CWORD]}))
+           return 0;;
+        *) return 1;;
+    esac
+}
+
+# tell bash to use the function to autocomplete epicsmng
+# if the function fails (return 1), fallback to default completion
+complete -o default -F _epicsmng_autocomplete_ epicsmng
