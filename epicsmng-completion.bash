a579154dde7f33fc5e3eb294086b41b844656c43
Davide Marcato <davide.marcato@lnl.infn.it>
2018-05-25 15:07:22 +0200

    Updated command list

--- epicsmng-completion.bash
+++ epicsmng-completion.bash
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
 
 # the commands available as first argument of epicsmng
-commands="makemodules cleanmodules listmodules createconf configureioc"
+commands="makemodules cleanmodules listmodules configureioc"
 
 #custom function to autocomplete epicsmng
 function _epicsmng_autocomplete_()
